b036727a5793135fd5f37ffc5d8fbd8d4806c360
Sridhar Ratnakumar <srid@srid.ca>
2026-03-19 16:06:08 -0400

    chore: unconditionally push to nix cache from all branches

--- vira.hs
+++ vira.hs
@@ -1,16 +1,11 @@
 -- Pipeline configuration for Vira <https://vira.nixos.asia/>
 
 \ctx pipeline ->
-  let
-    isMain = ctx.branch == "master"
-  in
   pipeline
     { build.systems =
         [ "x86_64-linux"
         , "aarch64-darwin"
         ]
     , signoff.enable = True
-    , cache.url = if
-        | isMain -> Just "https://cache.nixos.asia/oss"
-        | otherwise -> Nothing
+    , cache.url = Just "https://cache.nixos.asia/oss"
     }
