0a48842d442d49043abc97e3167779d572ad6d26
Sridhar Ratnakumar <srid@srid.ca>
2026-03-20 13:22:50 -0400

    fix: add curl to server nativeBuildInputs for utoipa-swagger-ui

--- server/crate.nix
+++ server/crate.nix
@@ -1,3 +1,8 @@
+{ pkgs, ... }:
 {
   autoWire = [ "crate" "clippy" ];
+  crane.args = {
+    # utoipa-swagger-ui downloads Swagger UI assets at build time via curl.
+    nativeBuildInputs = [ pkgs.curl ];
+  };
 }
