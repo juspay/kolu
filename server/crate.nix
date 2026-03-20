99d16a62e42ff0c0e3ea9db667c995d2dc0a9108
Sridhar Ratnakumar <srid@srid.ca>
2026-03-20 13:25:34 -0400

    fix: pre-fetch swagger-ui zip for Nix sandbox build

--- server/crate.nix
+++ server/crate.nix
@@ -1,8 +1,14 @@
 { pkgs, ... }:
+let
+  # Pre-fetch Swagger UI zip so utoipa-swagger-ui doesn't need network at build time.
+  swaggerUiZip = pkgs.fetchurl {
+    url = "https://github.com/swagger-api/swagger-ui/archive/refs/tags/v5.17.14.zip";
+    hash = "sha256-SBJE0IEgl7Efuu73n3HZQrFxYX+cn5UU5jrL4T5xzNw=";
+  };
+in
 {
   autoWire = [ "crate" "clippy" ];
   crane.args = {
-    # utoipa-swagger-ui downloads Swagger UI assets at build time via curl.
-    nativeBuildInputs = [ pkgs.curl ];
+    SWAGGER_UI_DOWNLOAD_URL = "file://${swaggerUiZip}";
   };
 }
