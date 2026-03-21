a43cce688432e4b9c5c45acdd73e94609fae4d8e
Sridhar Ratnakumar <srid@srid.ca>
2026-03-21 05:46:15 -0400

    pc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,10 @@ const App: Component = () => {
     <div class="flex flex-col h-screen bg-slate-900 text-white">
       <Header status={wsStatus()} />
       <div class="flex-1 min-h-0 p-2">
-        <div class="h-full rounded border border-slate-700 overflow-hidden p-2" style={{ "background-color": GHOSTTY_THEME.background }}>
+        <div
+          class="h-full rounded border border-slate-700 overflow-hidden p-2"
+          style={{ "background-color": GHOSTTY_THEME.background }}
+        >
           <TerminalView sessionId="default" onWsStatus={setWsStatus} />
         </div>
       </div>
