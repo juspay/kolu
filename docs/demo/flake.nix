16f007f802ea276bba6f8ba77c15187152826d3f
Sridhar Ratnakumar <srid@srid.ca>
2026-03-21 20:09:45 -0400

    refactor: use flake-parts for demo flake

--- docs/demo/flake.nix
+++ docs/demo/flake.nix
@@ -1,17 +1,17 @@
 {
   description = "kolu demo recording environment";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
 
-  outputs = { nixpkgs, ... }:
-    let
-      forAllSystems = f: nixpkgs.lib.genAttrs
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
-        (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             nodejs
             tsx
@@ -22,6 +22,6 @@
             export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
           '';
         };
-      });
+      };
     };
 }
