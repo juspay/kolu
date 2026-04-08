a34ac2bb6f45bd687912b2d531e3bf14d124162a
Sridhar Ratnakumar <srid@srid.ca>
2026-04-08 10:26:22 -0400

    chore: update pnpmDeps hash for @hono/node-server 1.19.13

--- default.nix
+++ default.nix
@@ -76,7 +76,7 @@ let
     pname = "kolu";
     version = "0.1.0";
     inherit src;
-    hash = "sha256-UznWxaO8ACIshOo5S/Trw+qJPFmRMOIstgA0+dKomyw=";
+    hash = "sha256-UorvSw67AVpO9j5bPg2Fs4B8vXrxxhBnwgtoe6HGVck=";
     fetcherVersion = 3;
   };
 
