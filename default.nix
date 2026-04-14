ce219aa785673f99eb68b47cf6a8f9682e63b1f9
Sridhar Ratnakumar <srid@srid.ca>
2026-04-14 09:07:52 -0400

    chore: nixpkgs-fmt default.nix

--- default.nix
+++ default.nix
@@ -37,7 +37,8 @@ let
     pname = "kolu";
     version = "0.1.0";
     inherit src;
-    hash = if pkgs.stdenv.isDarwin
+    hash =
+      if pkgs.stdenv.isDarwin
       then "sha256-uDUcuuFr9K01/SbJjlBnQ8xv5HWf/4oaUXEo2Ts1248="
       else "sha256-+0F68vGWwafQiKRThCuaCGYYDinYE0qwMhNd27UrPOI=";
     fetcherVersion = 3;
