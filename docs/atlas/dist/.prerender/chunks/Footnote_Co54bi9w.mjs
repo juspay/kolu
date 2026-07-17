089fc4cb569aeee3daad497190bbeeb7da083377
Sridhar Ratnakumar <srid@srid.ca>
2026-07-17 12:30:16 -0400

    docs(atlas): fix missing PrLink import (repairs previous commit's broken build)

--- docs/atlas/dist/.prerender/chunks/Footnote_Co54bi9w.mjs
+++
@@ -1,8 +0,0 @@
-import { E as maybeRenderHead, L as createComponent, f as renderSlot, h as renderTemplate } from "./server_B0R_ZhRD.mjs";
-import "./atlasGraph_BBFLFj6M.mjs";
-//#region src/components/Footnote.astro
-var $$Footnote = createComponent(($$result, $$props, $$slots) => {
-	return renderTemplate`${maybeRenderHead($$result)}<span class="fn" data-fn data-astro-cid-2yjzt2l5><button type="button" class="fn-ref" aria-expanded="false" aria-label="Footnote" data-astro-cid-2yjzt2l5><span class="fn-num" aria-hidden="true" data-astro-cid-2yjzt2l5></span></button><span class="fn-pop" role="tooltip" data-astro-cid-2yjzt2l5><span class="fn-pop-inner" data-astro-cid-2yjzt2l5>${renderSlot($$result, $$slots["default"])}</span></span></span>`;
-}, "/home/srid/code/kolu/.worktrees/RT-fable/docs/atlas/src/components/Footnote.astro", void 0);
-//#endregion
-export { $$Footnote as t };
